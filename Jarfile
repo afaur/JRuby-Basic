3b17ccf720da3eba8ce11bc63ef72d3e6241cf8f
Adam Faur <adam.faur@gmail.com>
2016-10-10 11:50:25 -0500

    Init webcam

--- Jarfile
+++ Jarfile
@@ -1,6 +1,1 @@
-jar 'org.yaml:snakeyaml', '1.14'
-jar 'bsf:bsf', '2.4.0'
-jar 'org.apache.solr:solr-solrj', '5.2.1'
-jar 'org.apache.solr:solr-core', '5.2.1'
-jar 'org.slf4j:slf4j-simple', '1.7.21'
-jar 'com.typesafe.akka:akka-actor_2.11', '2.4.4'
+jar 'com.github.sarxos:webcam-capture', '0.3.10'
--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,27 @@
-require './lib/classes/YamlReader.rb'
+require 'java'
+require 'jbundler'
 
-a = YamlReader.new('./config/test.yaml')
+#Java::Awt::Image::BufferedImage
+#Java::Io::File
+#Java::Io::IOException
+#Javax::Imageio::ImageIO
+#Com::Github::Sarxos::Webcam::Webcam
 
-puts a.data['person']
+java_import "com.github.sarxos.webcam.Webcam"
+java_import "javax.imageio.ImageIO"
+java_import "java.io.File"
+
+# get default webcam and open it
+#Com::Github::Sarxos::Webcam::Webcam
+webcam = Webcam.getDefault()
+
+# Open the camera
+webcam.open()
+
+# get image
+#Java::Awt::Image::BufferedImage
+image = webcam.getImage()
+
+# save image to PNG file
+ImageIO.write(image, "PNG", File.new("test.png"))
+
